803279c77bba5a6427d44086079de08a9222c7a9
JunhyeongJeon <sobu00@naver.com>
2021-04-04 03:22:33 +0900

    [refactor] Change indent

--- _utils.py
+++ _utils.py
@@ -2,13 +2,13 @@ import os
 import csv
 
 def make_dir(dir_name):
-	try:
-		if not(os.path.isdir(dir_name)):
-			os.makedirs(os.path.join(dir_name))
-	except OSError as e:
-		if e.errno != errno.EEXIST:
-			print("Failed to create directory!!!!!")
-			raise
+    try:
+        if not(os.path.isdir(dir_name)):
+            os.makedirs(os.path.join(dir_name))
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            print("Failed to create directory!!!!!")
+            raise
 
 def get_csv_file_name(csv_full_path):
     return csv_full_path.split("\\")[-1].split(".")[0]
@@ -44,6 +44,8 @@ KEYPOINT_LIST = ["nose", "left_eye", "right_eye", "left_ear", "right_ear",\
                 "left_knee", "right_knee", "left_ankle", "right_ankle",\
                 "neck", "left_palm", "right_palm","spine2(back)","spine1(waist)",\
                 "left_instep", "right_instep"]
+                
+
 
 MY_TEST = "TEST"
 COLOR_WHITE = (255, 255, 255)
