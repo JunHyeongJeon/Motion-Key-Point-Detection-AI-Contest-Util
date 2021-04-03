92f43aaee345c5bb205a81e73d286cb2d993701d
JunhyeongJeon <sobu00@naver.com>
2021-04-03 21:53:03 +0900

    [feat] Add utils function

---
+++ _utils.py
@@ -0,0 +1,8 @@
+def make_dir(dir_name):
+	try:
+		if not(os.path.isdir(dir_name)):
+			os.makedirs(os.path.join(dir_name))
+	except OSError as e:
+		if e.errno != errno.EEXIST:
+			print("Failed to create directory!!!!!")
+			raise
