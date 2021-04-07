98c2ff6eea6761e3a5ad4d5994f4c4fe538f8b1e
jhjeon <junhyeong_jeon@tmax.co.kr>
2021-04-07 21:49:15 +0900

    [refactor] postprocessing code

--- postprocessing_from_csv.py
+++ postprocessing_from_csv.py
@@ -1,11 +1,11 @@
 import os
-from _utils import(make_dir, write_output_csv_file, read_input_csv_file)
+from _utils import(make_dir, write_output_csv_file, read_input_csv_file, current_time)
 import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--csv_file_path', type=str, help=' CSV파일이 있는 경로',     
-        default='./csv/Sun-Apr--4-22_13_14-2021_output-wrist_fix.csv')
+        default='./ensemble/Wed-Apr--7-21_36_53-2021_output.csv')
     parser.add_argument('--output_folder_path', type=str, help='출력물이 있을 폴더',
         default='./csv_fix')
     return parser.parse_args()
@@ -14,16 +14,24 @@ def fix_palm_coordinate(csv_reader):
     output_list = []
     for index, row in enumerate(csv_reader):
         if float(row["left_elbow_y"]) < float(row["left_wrist_y"]):
-            row["left_palm_y"] = float(row["left_palm_y"]) + 20
+            row["left_palm_y"] = float(row["left_wrist_y"]) + 20
         else :
-            row["left_palm_y"] = float(row["left_palm_y"]) - 20
+            row["left_palm_y"] = float(row["left_wrist_y"]) - 20
         
         if float(row["right_elbow_y"]) < float(row["right_wrist_y"]):
-            row["right_palm_y"] = float(row["right_palm_y"]) + 20
+            row["right_palm_y"] = float(row["right_wrist_y"]) + 20
         else :
-            row["right_palm_y"] = float(row["right_palm_y"]) - 20
+            row["right_palm_y"] = float(row["right_wrist_y"]) - 20
         output_list.append(row)
 
+    return output_list
+
+def fix_instep_coordinate(csv_reader):
+    output_list = []
+    for index, row in enumerate(csv_reader):
+        row["left_instep_y"] = float(row["left_ankle_y"]) + 15
+        row["right_instep_y"] = float(row["right_ankle_y"]) + 15
+        output_list.append(row)
     return output_list
 
 if __name__ == '__main__':
@@ -32,6 +40,7 @@ if __name__ == '__main__':
     make_dir(args.output_folder_path)
 
     csv_list = read_input_csv_file(args.csv_file_path)
-    fix_list = fix_palm_coordinate(csv_list)
-    write_output_csv_file(os.path.join(args.output_folder_path, "test.csv"), fix_list)
+    fix_palm_list = fix_palm_coordinate(csv_list)
+    fix_list = fix_instep_coordinate(fix_palm_list)
+    write_output_csv_file(os.path.join(args.output_folder_path, current_time()+ "_test.csv"), fix_list)
     print("Done!")
